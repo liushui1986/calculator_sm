9cc78d430b6b15ffc621aadb7b5f4f4d911bdc55
liushui1986 <liushui1986@SHUAIPENG.localdomain>
2024-07-21 16:13:37 -0500

    add substract test

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -2,6 +2,9 @@ from app.src.calculator import Calculator
 
 cal = Calculator()
 
-def test_app():
+def test_add():
     assert cal.add(1, 1) == 2
     assert cal.add(1, 2) == 3
+
+def test_substract():
+    assert cal.substract(2, 1) == 1
